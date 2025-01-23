7e367d880db4349ce7feced97cbea195b92a8c71
xdaj96 <david@esetp760.edu.ar>
2025-01-23 11:57:33 -0300

    usando archivo env

--- apidescuentos/database.py
+++ apidescuentos/database.py
@@ -1,20 +1,38 @@
 from peewee import PostgresqlDatabase
- 
+from dotenv import load_dotenv
+
+import os 
+
+# Load the environment variables from the .env file
+load_dotenv()
+
 # Inicializamos la variable global de la base de datos, pero no la conectamos todavía
 # Configuración de la base de datos PostgreSQL
 db = PostgresqlDatabase(
-    'compras',
-    user='compras',
-    password='compras',
-    host='localhost',
+    os.getenv('DB_COMPRAS'),
+    user=os.getenv('DB_COMPRAS_USER'),
+    password=os.getenv('DB_COMPRAS_PASSWORD'),
+    host=os.getenv('HOST_COMPRAS'),
     port=5432
 )
 
 dbAutodw = PostgresqlDatabase(
-    'autofarmadw',
-    user='autofarmadw',
-    password='autofarmadw',
-    host='192.168.201.12',
+     os.getenv('DB_DW'),
+    user= os.getenv('DB_DW_USER'),
+    password= os.getenv('DB_DW_PASSWORD'),
+    host= os.getenv('HOST_DW'),
     port=5432
 )
 
+<<<<<<< Updated upstream
+=======
+# Base de datos para pruebas
+dbComprasDesarrollo = PostgresqlDatabase(
+     os.getenv('DB_COMPRAS'),
+    user= os.getenv('DB_COMPRAS'),
+    password= os.getenv('DB_COMPRAS'),
+    host=os.getenv('HOST_COMPRAS_DEVELOP'),
+    port=5432
+)
+
+>>>>>>> Stashed changes
